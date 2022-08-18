cf8156bb2dfc9784da3e115b05650943aa898a0b
Bensuperpc <bensuperpc@gmail.com>
2022-08-18 14:31:03 +0200

    Update class
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- ai.py
+++ ai.py
@@ -29,11 +29,7 @@ class AI:
     __compatibility__ = ["Linux", "Windows", "Darwin"]
     __name__ = "AI"
 
-    def __str__(self):
-        return "AI"
-
-    @staticmethod
-    def gpu():
+    def gpu(self):
         gpus = tf.config.list_physical_devices('GPU')
         if gpus:
             try:
@@ -47,20 +43,17 @@ class AI:
                 # Memory growth must be set before GPUs have been initialized
                 logger.error(e)
 
-    @classmethod
     def get_label(self, file_path):
         parts = tf.strings.split(file_path, os.path.sep)
-        one_hot = parts[-2] == self.class_names
+        one_hot = parts[-2] == self._class_names
 
         return tf.argmax(one_hot)
 
-    @classmethod
     def decode_img(self, img):
         img = tf.io.decode_jpeg(img, dct_method="INTEGER_ACCURATE", channels=3)
 
-        return tf.image.resize(img, [self.img_height, self.img_width])
+        return tf.image.resize(img, [self._img_height, self._img_width])
 
-    @classmethod
     def process_path(self, file_path):
         label = self.get_label(file_path)
         img = tf.io.read_file(file_path)
@@ -68,13 +61,12 @@ class AI:
 
         return img, label
 
-    @classmethod
     def configure_for_performance(self, ds):
         ds = ds.cache()
         ds = ds.shuffle(buffer_size=1000)
-        ds = ds.batch(self.batch_size)
+        ds = ds.batch(self._batch_size)
 
-        if self.data_augmentation:
+        if self._data_augmentation:
             logger.debug("Using data augmentation")
             data_augmentation = tf.keras.Sequential([
                 layers.RandomFlip("horizontal_and_vertical"),
@@ -84,12 +76,11 @@ class AI:
                 layers.RandomBrightness(0.1),
             ])
             ds = ds.map(lambda x, y: (data_augmentation(x, training=True), y),
-                        num_parallel_calls=self.AUTOTUNE)
+                        num_parallel_calls=self._AUTOTUNE)
 
-        ds = ds.prefetch(buffer_size=self.AUTOTUNE)
+        ds = ds.prefetch(buffer_size=self._AUTOTUNE)
         return ds
 
-    @classmethod
     def get_model(self):
         # data_augmentation = tf.keras.Sequential([
         #  layers.RandomFlip("horizontal_and_vertical"),
@@ -108,87 +99,86 @@ class AI:
             layers.Dropout(0.2),
             layers.Flatten(),
             layers.Dense(256, activation='relu'),
-            layers.Dense(len(self.class_names))
+            layers.Dense(len(self._class_names))
         ])
 
-        #model.build((None, self.img_height, self.img_width, 3))
+        #model.build((None, self._img_height, self._img_width, 3))
         # model.summary()
 
         return model
 
-    @classmethod
     def compile(self):
-        self.model.compile(
-            optimizer=self.optimizer,
+        self._model.compile(
+            optimizer=self._optimizer,
             loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
             metrics=['accuracy'])
 
-    @classmethod
     def load_model(self):
-        self.model = keras.models.load_model(self.model_path)
-        logger.debug(f"Model {self.model_path} loaded")
+        self._model = keras.models.load_model(self._model_path)
+        logger.debug(f"Model {self._model_path} loaded")
 
-    @classmethod
     def save_model(self):
-        self.model.save(self.model_path, save_format='h5')
-        logger.debug(f"Model saved to {self.model_path}")
+        self._model.save(self._model_path, save_format='h5')
+        logger.debug(f"Model saved to {self._model_path}")
 
-    @classmethod
     def prepare_train(self):
-        logger.debug(f"Loding data from {self.dataset_url}")
+        logger.debug(f"Data loaded from {self._data_dir}")
 
-        self.data_dir = pathlib.Path(tf.keras.utils.get_file(
-            'flower_photos', origin=self.dataset_url, untar=True))
+        self._class_names = np.array(sorted(
+            [item.name for item in self._data_dir.glob('*') if item.name != "LICENSE.txt"]))
 
-        self.class_names = np.array(sorted(
-            [item.name for item in self.data_dir.glob('*') if item.name != "LICENSE.txt"]))
-
-        logger.debug(f"Class names: {self.class_names}")
-        logger.debug(f"Number of classes: {len(self.class_names)}")
+        logger.debug(f"Class names: {self._class_names}")
+        logger.debug(f"Number of classes: {len(self._class_names)}")
 
         logger.debug(
-            f"Number of images: {len(list(self.data_dir.glob('*/*.jpg')))}")
+            f"Number of images: {len(list(self._data_dir.glob('*/*.jpg')))}")
 
-        self.list_ds = tf.data.Dataset.list_files(str(self.data_dir/'*/*'))
-        self.labeled_ds = self.list_ds.map(
-            self.process_path, num_parallel_calls=self.AUTOTUNE)
+        self._list_ds = tf.data.Dataset.list_files(str(self._data_dir/'*/*'))
+        self._labeled_ds = self._list_ds.map(
+            self.process_path, num_parallel_calls=self._AUTOTUNE)
 
-        train_size = int(self.train_pourcent * len(self.labeled_ds))
-        val_size = int(self.val_pourcent * len(self.labeled_ds))
-        test_size = int(self.test_pourcent * len(self.labeled_ds))
-        self.train_ds = self.labeled_ds.take(train_size)
-        self.val_ds = self.labeled_ds.skip(train_size)
-        self.val_ds = self.val_ds.take(val_size)
-        self.test_ds = self.labeled_ds.skip(train_size + val_size)
-        self.test_ds = self.test_ds.take(test_size)
+        train_size = int(self._train_pourcent * len(self._labeled_ds))
+        val_size = int(self._val_pourcent * len(self._labeled_ds))
+        test_size = int(self._test_pourcent * len(self._labeled_ds))
 
-        self.train_ds = self.configure_for_performance(self.train_ds)
-        self.val_ds = self.configure_for_performance(self.val_ds)
-        self.test_ds = self.configure_for_performance(self.test_ds)
+        self._train_ds = self._labeled_ds.take(train_size)
+
+        self._val_ds = self._labeled_ds.skip(train_size)
+        self._val_ds = self._val_ds.take(val_size)
+
+        self._test_ds = self._labeled_ds.skip(train_size + val_size)
+        self._test_ds = self._test_ds.take(test_size)
+
+        logger.debug(f"Train size: {len(self._train_ds)}")
+        logger.debug(f"Val size: {len(self._val_ds)}")
+        logger.debug(f"Test size: {len(self._test_ds)}")
+
+        self._train_ds = self.configure_for_performance(self._train_ds)
+        self._val_ds = self.configure_for_performance(self._val_ds)
+        self._test_ds = self.configure_for_performance(self._test_ds)
 
         # Load model
-        if self.model is None:
+        if self._model is None:
             logger.warning("Model is None, load default model")
-            self.model = self.get_model()
+            self._model = self.get_model()
 
-            self.model.pop()
-            self.model.add(layers.Dense(len(self.class_names)))
-            #self.model.build((None, self.img_height, self.img_width, 3))
-            # self.model.summary()
+            self._model.pop()
+            self._model.add(layers.Dense(len(self._class_names)))
+            #self._model.build((None, self._img_height, self._img_width, 3))
+            # self._model.summary()
 
-    @classmethod
     def train(self):
         logger.debug("Start training")
-        self.history = self.model.fit(
-            self.train_ds,
-            validation_data=self.val_ds,
-            epochs=self.epochs
+        logger.debug(f"Epochs: {self._epochs}")
+        self._history = self._model.fit(
+            self._train_ds,
+            validation_data=self._val_ds,
+            epochs=self._epochs
         )
 
-    @classmethod
     def evaluate(self):
         logger.debug("Start evaluation")
-        loss, accuracy = self.model.evaluate(self.test_ds)
+        loss, accuracy = self._model.evaluate(self._test_ds)
 
         logger.debug(f"Loss: {loss * 100} %")
         logger.debug(f"Accuracy: {accuracy * 100} %")
@@ -196,11 +186,10 @@ class AI:
         return loss, accuracy
 
     # TODO : Need to be fixed
-    @classmethod
     def predict(self, img_path):
         logger.debug("Start prediction")
         # img = keras.preprocessing.image.load_img(
-        #    img_path, target_size=(self.img_height, self.img_width)
+        #    img_path, target_size=(self._img_height, self._img_width)
         # )
         #img_array = keras.preprocessing.image.img_to_array(img)
         #img_array = tf.expand_dims(img_array, 0)
@@ -209,26 +198,25 @@ class AI:
 
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
-        image = cv.resize(image, (self.img_height, self.img_width))
+        image = cv.resize(image, (self._img_height, self._img_width))
 
         image_tensor = tf.convert_to_tensor(image, dtype=tf.float32)
         image_tensor = tf.expand_dims(image_tensor, 0)
 
-        predictions = self.model.predict(image_tensor)
+        predictions = self._model.predict(image_tensor)
 
         logger.debug(
-            f"Predictions: {self.class_names[predictions]}")
+            f"Predictions: {self._class_names[predictions]}")
         return predictions
 
-    @classmethod
     def display_history(self):
-        acc = self.history.history['accuracy']
-        val_acc = self.history.history['val_accuracy']
+        acc = self._history.history['accuracy']
+        val_acc = self._history.history['val_accuracy']
 
-        loss = self.history.history['loss']
-        val_loss = self.history.history['val_loss']
+        loss = self._history.history['loss']
+        val_loss = self._history.history['val_loss']
 
-        epochs_range = range(self.epochs)
+        epochs_range = range(self._epochs)
 
         plt.figure(figsize=(8, 8))
         plt.subplot(1, 2, 1)
@@ -244,7 +232,6 @@ class AI:
         plt.title('Training and Validation Loss')
         plt.show()
 
-    @classmethod
     def plot_image(self, predictions_array, true_label, img, grid=False, pred_color='red', true_color='blue'):
         plt.grid(grid)
         plt.xticks([])
@@ -257,28 +244,26 @@ class AI:
             color = true_color
         else:
             color = pred_color
-        plt.xlabel("{} {:2.0f}% ({})".format(self.class_names[predicted_label],
+        plt.xlabel("{} {:2.0f}% ({})".format(self._class_names[predicted_label],
                                              100*np.max(predictions_array),
-                                             self.class_names[true_label]),
+                                             self._class_names[true_label]),
                    color=color)
 
-    @classmethod
     def plot_value_array(self, predictions_array, true_label, grid=False, pred_color='red', true_color='blue'):
         plt.grid(grid)
-        plt.xticks(range(len(self.class_names)))
+        plt.xticks(range(len(self._class_names)))
         plt.yticks([])
-        thisplot = plt.bar(range(len(self.class_names)),
+        thisplot = plt.bar(range(len(self._class_names)),
                            predictions_array, color="#777777")
         plt.ylim([0, 1])
         predicted_label = np.argmax(predictions_array)
         thisplot[predicted_label].set_color(pred_color)
         thisplot[true_label].set_color(true_color)
 
-    @classmethod
     def display_predict(self):
-        image_batch, label_batch = next(iter(self.test_ds))
+        image_batch, label_batch = next(iter(self._test_ds))
 
-        probability_model = tf.keras.Sequential([self.model,
+        probability_model = tf.keras.Sequential([self._model,
                                                 tf.keras.layers.Softmax()])
         predictions = probability_model.predict(image_batch)
 
@@ -300,44 +285,74 @@ class AI:
         plt.tight_layout()
         plt.show()
 
-    @classmethod
-    def __init__(self, *args, **kwargs):
+    # Defining __init__ method
+    def __init__(self):
+        self.__name = "Not Set"
         logger.debug(f"TF version: {tf.__version__}")
 
-        self.batch_size = 24
-        self.img_height = 256
-        self.img_width = 256
-        self.epochs = 10
+        self._batch_size = 24
+        self._img_height = 256
+        self._img_width = 256
+        self._epochs = 10
 
-        self.AUTOTUNE = tf.data.AUTOTUNE
+        self._AUTOTUNE = tf.data.AUTOTUNE
 
-        self.class_names = []
+        self._class_names = []
 
-        self.dataset_url = ""
-        self.data_dir = None
-        self.data_augmentation = False
+        self._data_dir = None
+        self._data_augmentation = False
 
-        self.list_ds = None
-        self.train_ds = None
-        self.train_ds = None
-        self.val_ds = None
-        self.val_ds = None
-        self.test_ds = None
-        self.test_ds = None
+        self._list_ds = None
+        self._train_ds = None
+        self._val_ds = None
+        self._test_ds = None
 
-        self.train_pourcent = 0.8
-        self.val_pourcent = 0.1
-        self.test_pourcent = 0.1
+        self._train_pourcent = 0.8
+        self._val_pourcent = 0.1
+        self._test_pourcent = 0.1
 
-        self.history = None
+        self._history = None
 
-        self.model = None
+        self._model = None
         # tf.keras.optimizers.Adam(learning_rate=1e-4)
-        self.optimizer = "adam"
-        self.model_path = "model.h5"
+        self._optimizer = "adam"
+        self._model_path = "model.h5"
 
-        # Init GPU if available
-        self.gpu()
+    @property
+    def data_dir(self):
+        return self._data_dir
+
+    @data_dir.setter
+    def name(self, val):
+        self._data_dir = val
+
+    @name.deleter
+    def data_dir(self):
+        del self._data_dir
+
+    @property
+    def epochs(self):
+        return self._epochs
+
+    @epochs.setter
+    def name(self, val):
+        self._epochs = val
+
+    @name.deleter
+    def epochs(self):
+        del self._epochs
+
+    @property
+    def batch_size(self):
+        return self.__batch_size
+
+    @batch_size.setter
+    def batch_size(self, val):
+        self._batch_size = val
+
+    @batch_size.deleter
+    def batch_size(self):
+        del self._batch_size
 
 
 if __name__ == '__main__':
@@ -346,11 +361,26 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--test', action='store_false')
     args = parser.parse_args()
 
+    # Init AI
     ai = AI()
-    ai.dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+
+    # Enable GPU
+    ai.gpu()
+
+    data_dir = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+
+    ai.data_dir = pathlib.Path(tf.keras.utils.get_file(
+        'flower_photos', origin=data_dir, untar=True))
+
+    logger.debug(f"Data dir: {ai.data_dir}")
+
     ai.prepare_train()
     ai.compile()
+
+    ai.epochs = 9
+
     ai.train()
+
     ai.evaluate()
 
     if args.test:
